0b1034cc42e84301790cb1d1e5193e7cd450f5f4
Costin Bernhart <e11816878@student.tuwien.ac.at>
2022-04-05 12:28:32 +0200

    [UPDATE]: use standard sklearn normalize function

--- src/faceRecognition/distanceMetrics.py
+++ src/faceRecognition/distanceMetrics.py
@@ -4,7 +4,7 @@ cosine distance, L2,
 """
 import numpy as np
 from sklearn.metrics.pairwise import euclidean_distances, cosine_distances
-
+from sklearn.preprocessing import normalize
 
 def cosineDistance(features1, features2):
     """ calculates the cosine distance between two feature vectors. """
@@ -14,6 +14,6 @@ def euclideanDistance(features1,features2):
     """calulates the euclidean distance between two feature vectors"""
     f1 = features1.reshape(1,-1)
     f2 = features2.reshape(1,-1)
-    n1 = f1 / np.linalg.norm(f1)
-    n2 = f2 / np.linalg.norm(f2)
+    n1 = normalize(f1)
+    n2 = normalize(f2)
     return euclidean_distances(n1,n2).item()
