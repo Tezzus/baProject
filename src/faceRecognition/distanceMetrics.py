b9c97ca431cb6ce18f12721026c588672c880fb4
Costin Bernhart <e11816878@student.tuwien.ac.at>
2022-04-05 12:05:32 +0200

    [UPDATE]: use standard sklearn library for distance calculation

--- src/faceRecognition/distanceMetrics.py
+++ src/faceRecognition/distanceMetrics.py
@@ -3,34 +3,17 @@ has 2 different distance metrics for evaluating the similiarity between to face 
 cosine distance, L2,
 """
 import numpy as np
+from sklearn.metrics.pairwise import euclidean_distances, cosine_distances
+
 
 def cosineDistance(features1, features2):
     """ calculates the cosine distance between two feature vectors. """
-    feature1 = features1.cpu().numpy()
-    feature2 = features2.cpu().numpy()
-    n1 = np.linalg.norm(feature1)
-    n2 = np.linalg.norm(feature2)
-    x1 = feature1 / n1
-    x2 = feature2 / n2
-    return 1 - np.dot(x1,x2)
-
-"""TODO Problem EUCLIDEAN DISTANCE"""
+    return cosine_distances(features1.reshape(1, -1),features2.reshape(1, -1)).item()
 
 def euclideanDistance(features1,features2):
     """calulates the euclidean distance between two feature vectors"""
-    ###with vector normalisation.
-    feature1 = features1.detach().cpu()
-    feature2 = features2.detach().cpu()
-    n1 = np.linalg.norm(feature1)
-    n2 = np.linalg.norm(feature2)
-
-    x1 = feature1 / n1
-    x2 = feature2 / n2
-    return (x1 - x2).norm().item()
-    """ With numpy
-    feature1 = features1.cpu().numpy()
-    feature2 = features2.cpu().numpy()
-    diff = np.subtract(feature1, feature2)
-
-    return np.linalg.norm(diff)
-    """
+    f1 = features1.reshape(1,-1)
+    f2 = features2.reshape(1,-1)
+    n1 = f1 / np.linalg.norm(f1)
+    n2 = f2 / np.linalg.norm(f2)
+    return euclidean_distances(n1,n2).item()
